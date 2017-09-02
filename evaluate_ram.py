7fe777628d19a738cd43b1558f254bb6c569d9c1
Yu Gai <yg1246@nyu.edu>
2017-09-02 18:41:29 -0400

    sync

--- evaluate_ram.py
+++ evaluate_ram.py
@@ -1,29 +1,23 @@
-from pdb import set_trace as st
 from argparse import ArgumentParser
-from random import randint
-import numpy as np
 import torch as th
 from torch.autograd import Variable
-import torch.nn.functional as F
 from visdom import Visdom
-from networks import MLP, Network0
+from ram import RAM
 from utilities import *
-from visualizer import TraceVisualizer, ImageVisualizer
+from visualizer import TraceVisualizer
 
 parser = ArgumentParser()
 parser.add_argument('--batch-size', type=int, default=64)
 parser.add_argument('--gpu', type=int, default=-1)
+parser.add_argument('--h', type=int, default=4)
 parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--mask-vis-height', type=int, default=100)
-parser.add_argument('--mask-vis-interval', type=int, default=100)
-parser.add_argument('--mask-vis-width', type=int, default=100)
 parser.add_argument('--mnist-path', type=str, default='mnist.dat')
 parser.add_argument('--n-epochs', type=int, default=100)
-parser.add_argument('--n-features', type=int, default=64)
-parser.add_argument('--nonlinear', action=parse_nonlinear, default=F.relu)
-parser.add_argument('--penalty', type=float, default=1)
+parser.add_argument('--n-scales', type=int, default=1)
+parser.add_argument('--sx', type=float, default=0.1)
+parser.add_argument('--sy', type=float, default=0.1)
 parser.add_argument('--T', type=int, default=4)
-parser.add_argument('--units', action=partition('-', int), default=(256, ))
+parser.add_argument('--w', type=int, default=4)
 args = parser.parse_args()
 print args
 
@@ -35,10 +29,7 @@ else:
 
 loader_dict, size = create_mnist_loaders(args.mnist_path, args.batch_size)
 
-n_pixels = size[0] * size[1]
-feature_extractor = MLP(n_pixels, args.n_features, args.units, args.nonlinear)
-model = Network0(feature_extractor, args.n_features, size, args.T,
-                 args.penalty)
+model = RAM(args)
 if cuda:
     model.cuda()
 optimizer = th.optim.Adam(model.parameters(), args.lr)
@@ -48,34 +39,18 @@ tl_vis = TraceVisualizer(visdom, {'title': 'training loss'})
 ta_vis = TraceVisualizer(visdom, {'title': 'training accuracy'})
 va_vis = TraceVisualizer(visdom, {'title': 'validation accuracy'})
 
-opts = {'width': args.mask_vis_width, 'height': args.mask_vis_height}
-mask_vis_tuple = tuple(ImageVisualizer(visdom, opts) for _ in range(args.T))
-
-
-def vis_mask(internal):
-    data = internal['data']
-    N = data.size()[0]
-    index = randint(0, N - 1)
-    data = data.data[index].numpy()
-    data = np.reshape(data, (28, 28))
-    mask_list = internal['mask_list']
-    mask_list = list(mask.data[index].numpy() for mask in mask_list)
-    mask_list = map(np.squeeze, mask_list)
-    for mask, vis in zip(mask_list, mask_vis_tuple):
-        vis.visualize(mask * data)
-
-
 for epoch in range(args.n_epochs):
     print 'epoch %d' % epoch
 
     tllist_list, talist_list = [], []
     for iteration, batch in enumerate(loader_dict['train']):
         data, labels = batch
+        data = data.view(args.batch_size, 1, *size)
         if cuda:
             data, labels = data.cuda(), labels.cuda()
         data, labels = Variable(data), Variable(labels)
-        prediction_list, internal = model(data)
-        loss, ce_list = model.loss(prediction_list, labels, internal)
+        prediction_list, cache_list = model(data)
+        loss, ce_list = model.loss(prediction_list, cache_list, labels)
         tllist_list.append(ce_list)
 
         optimizer.zero_grad()
@@ -84,9 +59,6 @@ for epoch in range(args.n_epochs):
 
         accuracy_list = accuracy(prediction_list, labels)
         talist_list.append(accuracy_list)
-
-        if (iteration + 1) % args.mask_vis_interval == 0:
-            vis_mask(internal)
 
     for i, tllist in enumerate(zip(*tllist_list)):
         label = 'iteration %d' % i
---
+++ ram.py
@@ -0,0 +1,166 @@
+from __future__ import division
+import numpy as np
+import tensorflow as tf
+import torch as th
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from utilities import cross_entropy
+
+
+class CoreNetwork(nn.Module):
+    def __init__(self):
+        super(CoreNetwork, self).__init__()
+        self._h_linear = nn.Linear(256, 256)
+        self._g_linear = nn.Linear(256, 256)
+
+    def forward(self, h, g):
+        h = self._h_linear(h)
+        g = self._g_linear(g)
+        h = F.relu(h + g)
+        return h
+
+
+class GlimpseNetwork(nn.Module):
+    def __init__(self, args):
+        super(GlimpseNetwork, self).__init__()
+        self._retina_size = args.n_scales * args.w * args.h
+        self._retina_linear = nn.Linear(self._retina_size, 128)
+        self._location_linear = nn.Linear(2, 128)
+        self._hg_linear = nn.Linear(128, 256)
+        self._hl_linear = nn.Linear(128, 256)
+
+    def forward(self, retina, location):
+        retina = retina.view(-1, self._retina_size)
+        hg = F.relu(self._retina_linear(retina))
+        hg = self._hg_linear(hg)
+
+        hl = F.relu(self._location_linear(location))
+        hl = self._hl_linear(hl)
+
+        g = F.relu(hg + hl)
+        return g
+
+
+class LocationNetwork(nn.Module):
+    def __init__(self, args):
+        super(LocationNetwork, self).__init__()
+        self._linear = nn.Linear(256, 2)
+        self._sx, self._sy = args.sx, args.sy
+
+    def forward(self, h):
+        mean = self._linear(h)
+        mx, my = th.chunk(mean, 2, 1)
+        sx = th.ones(mx.size()) * self._sx
+        sy = th.ones(my.size()) * self._sy
+        if mean.is_cuda:
+            sx, sy = sx.cuda(), sy.cuda()
+        sx, sy = Variable(sx), Variable(sy)
+        x, y = th.normal(mx, sx), th.normal(my, sy)
+        x, y = x.detach(), y.detach()
+        location = th.cat((x, y), 1)
+        cache = ((mx, my, sx, sy), (x, y))
+        return location, cache
+
+    def loss(self, reward, cache):
+        """
+        Parameters
+        ----------
+        reward : N by 1 tensor
+        cache : tuple
+        """
+
+        if cache is None:
+            return 0
+
+        (mx, my, sx, sy), (x, y) = cache
+        log_px = -(x - mx) ** 2 / (2 * sx ** 2)
+        log_py = -(y - my) ** 2 / (2 * sy ** 2)
+        log_p = log_px + log_py
+        value = th.mean(log_p * reward)
+        return value
+
+
+class RetinaEncoder(nn.Module):
+    def __init__(self, args):
+        super(RetinaEncoder, self).__init__()
+        self._patch_size = np.array([args.w, args.h])
+        self._n_scales = args.n_scales
+
+        with tf.device('/cpu:0'):
+            input = tf.placeholder("float32", None, 'input')
+            glimpse_size = tf.placeholder("int32", None, 'glimpse_size')
+            patch_size = tf.Variable(self._patch_size, False)
+            offsets = tf.placeholder("float32", None, 'offsets')
+            glimpse = tf.image.extract_glimpse(input, glimpse_size, offsets)
+            self._glimpse = tf.image.resize_images(glimpse, self._patch_size)
+
+    def forward(self, input, offsets):
+        cuda = input.is_cuda
+        input, offsets = input.data.cpu().numpy(), offsets.data.cpu().numpy()
+        input = np.transpose(input, (0, 3, 2, 1))
+        feed_dict = {
+            'input:0': input,
+            'offsets:0': offsets,
+            'glimpse_size:0': self._patch_size
+        }
+        glimpse_list = []
+        with tf.Session() as s:
+            s.run(tf.global_variables_initializer())
+            for _ in range(self._n_scales):
+                glimpse = s.run(self._glimpse, feed_dict)
+                feed_dict['glimpse_size:0'] *= 2
+                glimpse = np.transpose(glimpse, (0, 3, 2, 1))
+                glimpse = th.from_numpy(glimpse)
+                if cuda:
+                    glimpse = glimpse.cuda()
+                glimpse_list.append(glimpse)
+        retina = th.cat(glimpse_list, 1)
+        retina = Variable(retina)
+        return retina
+
+
+class RAM(nn.Module):
+    def __init__(self, args):
+        super(RAM, self).__init__()
+        self._classifier = nn.Linear(256, 10)
+        self._core_network = CoreNetwork()
+        self._glimpse_network = GlimpseNetwork(args)
+        self._location_network = LocationNetwork(args)
+        self._retina_encoder = RetinaEncoder(args)
+
+        self._T = args.T
+
+    def forward(self, data):
+        N = data.size()[0]
+        location, cache = (th.rand(N, 2) - 0.5) * 2, None
+        h = th.zeros(N, 256)
+        if data.is_cuda:
+            location, h = location.cuda(), h.cuda()
+        location, h = Variable(location), Variable(h)
+        prediction_list, cache_list = [], []
+        for _ in range(self._T):
+            retina = self._retina_encoder(data, location)
+            glimpse = self._glimpse_network(retina, location)
+            h = self._core_network(h, glimpse)
+            category = self._classifier(h)
+            prediction_list.append(category)
+            cache_list.append(cache)
+            location, cache = self._location_network(h)
+
+        return prediction_list, cache_list
+
+    def loss(self, prediction_list, cache_list, label):
+        ce_tuple = tuple(cross_entropy(prediction_list, label))
+        ce_loss = sum(ce_tuple)
+
+        argmax = lambda t: th.max(t, 1)[1]
+        category_tuple = tuple(argmax(p) for p in prediction_list)
+        indicator_tuple = tuple(c == label for c in category_tuple)
+        reward, rl_loss = 0, 0
+        for indicator, cache in reversed(zip(indicator_tuple, cache_list)):
+            reward = reward + indicator
+            rl_loss = rl_loss + self._location_network.loss(reward.float(), cache)
+
+        value = (ce_loss + rl_loss) / self._T
+        return value, ce_tuple
